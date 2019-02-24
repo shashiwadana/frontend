5a8ae7fea25079992c7b61dbc02ac4cb3fb287bd
zbuddy <shashiwadananirmani14@gmail.com>
2019-02-24 23:55:18 +0530

    signup

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public form={
+    email:null,
+    password:null
+  };
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  onSubmit(){
+ return this.http.post('http://localhost:8000/api/auth/login',this.form).subscribe(
+   data=>console.log(data),
+   error=>console.log(error)
+ );
+  }
 
   ngOnInit() {
   }
